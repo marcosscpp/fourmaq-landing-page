296c1da353217ae9a40223f075dfa6b01a2f2a39
Marcos <marcosscpp@gmail.com>
2025-07-28 21:46:15 -0300

    Finishing Benefits Section

---
+++ scripts/maquinas-agricolas.js
@@ -0,0 +1,27 @@
+import triggerEvent from "./modules/trigger-event";
+import SmoothScroll from "./modules/smooth-scroll";
+
+import Form from "./modules/form";
+import Modal from "./modules/modal";
+import initHotjar from "./init-hotjar";
+
+new SmoothScroll("a[href^='#']").init();
+
+triggerEvent("PageView");
+
+initHotjar();
+
+const form = new Form(".contact__form");
+
+const modal = new Modal(
+  "[data-modal='open']",
+  "[data-modal='close']",
+  "[data-modal='container']"
+).init();
+
+const formInternalLinks = document.querySelectorAll("[href='#contato']");
+formInternalLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    triggerEvent("Clicou no link do formulário");
+  });
+});
